09535a6e9320bab59c46dd9fdfa95ffdecf2b5e9
TyperGuy <ray.developper@gmail.com>
2022-10-23 00:13:14 +0100

    feat: add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,14 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import "@fontsource/nunito-sans";
 import theme from "../themes/myTheme";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <title>ShadowGhost</title>
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
