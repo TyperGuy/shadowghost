c9fa00645978e8941b1c57712ce4522fd8b53fd3
TyperGuy <ray.developper@gmail.com>
2022-10-22 22:43:02 +0100

    chore(theme): setup basic project configuration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
 import type { NextPage } from "next";
 
-import styles from "../styles/Home.module.css";
-
 const Home: NextPage = () => {
-  return <div className={styles.container}>Isso é um template</div>;
+  return <div>Isso é um template</div>;
 };
 
 export default Home;
