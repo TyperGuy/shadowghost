2b981b28bda00e027ad26e52163b42b0ccb951e8
TyperGuy <ray.developper@gmail.com>
2022-10-25 13:47:41 +0100

    feat(widget): create custom box-shadow modal

--- views/call2action/index.tsx
+++ views/call2action/index.tsx
@@ -1,10 +1,13 @@
-import { Box, Button, Flex, Img, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Img, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, useDisclosure } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import Styles from "./Call2Action.module.scss";
+import {BiCopyAlt as Copy} from 'react-icons/bi';
 
 
 export const Call2Action: NextPage = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
+    <>
     <div className={Styles.container}>
      <div className={Styles.innerContainer}>
       <Flex direction="column" gap={10}>
@@ -14,11 +17,42 @@ export const Call2Action: NextPage = () => {
         </Text>
         <Flex gap={5}className={Styles.buttonContainer}>
           <Button className={Styles.button}> Expore existing</Button>
-          <Button className={Styles.button}> Eazy Create</Button>
+          <Button onClick={onOpen} className={Styles.button}> Eazy Create</Button>
         </Flex>
       </Flex>
       <Img className={Styles.img} src="/illustration.png"/>
      </div>
     </div>
+    <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Create Custom Box-Shadow</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae perspiciatis reiciendis voluptate, totam consequuntur quisquam eius, voluptatem fugiat dicta corrupti rerum. Magnam, inventore eos ea odio tenetur laudantium itaque voluptatem.
+          </ModalBody>
+
+          <ModalFooter>
+          <Button 
+            leftIcon={
+              <Flex 
+                width="25px"
+                height="25px" 
+                borderRadius="50%" 
+                justify="center" 
+                align="center"  
+                bg="#fff">
+                  <Copy 
+                    color="#222"/>
+              </Flex>} 
+                className={Styles.copyButton}
+                > Copy to clipboard
+          </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
+
+
